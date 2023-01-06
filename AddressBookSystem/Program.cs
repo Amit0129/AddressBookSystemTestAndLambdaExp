b9cda770e57642ab49aa5c5121d54d00ce2064e2
Amit0129 <amitkumarnayak40@gmail.com>
2023-01-06 18:34:43 +0530

    Amit]Adding Contacts to Address Book

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -5,9 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            Console.WriteLine("Welcome to Address Book System");
-            ContactsRecord record = new ContactsRecord();
-            record.Input();
+            AddressBook records = new AddressBook();
+            Console.WriteLine("1. Add a new Record");
+            Console.WriteLine("Enter your choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    records.AddRecords();
+                    records.PrintRecords();
+                    Console.ReadLine();
+                    break;
+            }
         }
     }
 }
